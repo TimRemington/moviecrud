c8fa3b06608d8e4ce9f7cb9070e261500f15f7b5
Tim Remington <timremington@Tims-MacBook-Pro.local>
2018-10-24 18:08:16 -0600

    Getting Heroku to work

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
   test: {},
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://sgbizhlzinahsl:30d26db6d03806b38d8918395b5f4f4db4972ccf12f1c55801a5c52e8656ec15@ec2-54-225-115-234.compute-1.amazonaws.com:5432/d6qdu7dioppnqo'
   }
 }
